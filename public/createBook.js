c74deccb4c3c81b61aaf95354c87ede4098f4acd
dean <iamdekanos@gmail.com>
2021-06-11 23:11:29 +0300

    Added some comments

--- public/createBook.js
+++ public/createBook.js
@@ -3,16 +3,18 @@ const form = document.querySelector("form");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  // Getting the values from the form
   const author = form.author.value;
   const title = form.title.value;
   const date = form.date.value;
 
   await fetch("/api/v1/createbook", {
+    // Making a GET request to the createbook endpoint
     method: "POST",
     body: JSON.stringify({ author, title, date }),
     headers: { "Content-Type": "application/json" },
   })
-    .then((result) => result.json())
+    .then((result) => result.json()) // Parsing the object sent back from the server to JSON and using it
     .then((newResult) => console.log(newResult))
     .catch((err) => console.log(err));
 
--- public/index.js
+++ public/index.js
@@ -3,5 +3,5 @@ const response = fetch("/api/v1/books", {
   headers: { "Content-Type": "application/json" },
 })
   .then((response) => response.json())
-  .then((newResponse) => console.log(newResponse)) //newResponse.books.forEach((book) => console.log(book)))
+  .then((newResponse) => console.log(newResponse))
   .catch((err) => console.log(err));
